aac9d0ce5e170c8c4ed0a5ba793eaf95e1698223
Huakun <huakun.shen@huakunshen.com>
2026-09-15 04:13:28 +0000

    bread 0.2.4

--- Formula/bread.rb
+++ Formula/bread.rb
@@ -4,26 +4,26 @@
 class Bread < Formula
   desc "Bounded batch file reader for coding agents"
   homepage "https://github.com/HuakunShen/bread"
-  version "0.2.3"
+  version "0.2.4"
   license "MIT"
 
   if OS.mac?
     if Hardware::CPU.arm?
-      url "https://github.com/HuakunShen/bread/releases/download/v0.2.3/bread_0.2.3_darwin_arm64.tar.gz"
-      sha256 "c4d2fb055583542e9f09174fd7b9686183fac0a7cab373cdaec6bc6678a07f6b"
+      url "https://github.com/HuakunShen/bread/releases/download/v0.2.4/bread_0.2.4_darwin_arm64.tar.gz"
+      sha256 "b0a34a1021dc4f98caa44f6554122a4ddb7db3232a35769750c9b9d9a4578a47"
     elsif Hardware::CPU.intel?
-      url "https://github.com/HuakunShen/bread/releases/download/v0.2.3/bread_0.2.3_darwin_amd64.tar.gz"
-      sha256 "6e91ce48cadfe6f6bddd4de5bda5f93f862c09e4699a2ee70dde030e6baf54bd"
+      url "https://github.com/HuakunShen/bread/releases/download/v0.2.4/bread_0.2.4_darwin_amd64.tar.gz"
+      sha256 "1bffa4a1e30687fa64a4541023aa582d6e91ab461513c8ee1992e5bd7b549083"
     else
       raise "Unsupported macOS architecture"
     end
   elsif OS.linux?
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/HuakunShen/bread/releases/download/v0.2.3/bread_0.2.3_linux_arm64.tar.gz"
-      sha256 "d942b8044bfa55a37ec71e63f12fe7e926b7a44db1d771f9d4a9d710f722e423"
+      url "https://github.com/HuakunShen/bread/releases/download/v0.2.4/bread_0.2.4_linux_arm64.tar.gz"
+      sha256 "77d467cfcb61d7182d8077414383b7e11a771cad7ec287ccb963c07d699cd0d1"
     elsif Hardware::CPU.intel? && Hardware::CPU.is_64_bit?
-      url "https://github.com/HuakunShen/bread/releases/download/v0.2.3/bread_0.2.3_linux_amd64.tar.gz"
-      sha256 "2995f716a755afe4612283596793143c8f1ec74d2c5bfc8319a034498dc2894c"
+      url "https://github.com/HuakunShen/bread/releases/download/v0.2.4/bread_0.2.4_linux_amd64.tar.gz"
+      sha256 "d945689f7c5f4ee013039c38167a5cdc399ba42902f3cdf84c00ed8264a45b7b"
     else
       raise "Unsupported Linux architecture"
     end
